802228ab8b9a421c2b008760a86ec2ccc51fbfaa
OTerrien <oterrien@neuf.fr>
2017-05-25 13:53:29 +0200

    update with retry

--- client/src/main/java/com/ote/test/Client.java
+++ client/src/main/java/com/ote/test/Client.java
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRetry
 public class Client {
 
     public static void main(String[] args) {
--- client/src/main/java/com/ote/test/LoadBalancerRestController.java
+++ client/src/main/java/com/ote/test/LoadBalancerRestController.java
@@ -3,10 +3,14 @@ package com.ote.test;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * client tries to loadbalance the service (it does not use the gateway for that)
+ */
 @RestController
 @RequestMapping("/loadBalance")
 @Slf4j
@@ -19,6 +23,7 @@ public class LoadBalancerRestController {
     private RestTemplate restTemplate;
 
     @RequestMapping("/service/home")
+    @Retryable
     public String homeRemote() {
 
         log.info("Choose a service (load balancer) then call its endpoint");
